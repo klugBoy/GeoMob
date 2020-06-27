0a2945bf273a80a54440aee32710baa2de7e9d82
Mahi Begoug <gm_begoug@esi.dz>
2020-06-27 20:58:26 +0100

    add Ressource Model

--- app/src/main/java/com/example/corona/geomob/data/DAOs/RessourceDAO.kt
+++ app/src/main/java/com/example/corona/geomob/data/DAOs/RessourceDAO.kt
@@ -1,4 +1,27 @@
 package com.example.corona.geomob.data.DAOs
 
+import androidx.room.*
+import com.example.corona.geomob.data.Domaines.Personnalite
+import com.example.corona.geomob.data.Domaines.Ressource
+
+
+@Dao
 interface RessourceDAO {
+    @Insert
+    suspend fun addRessource(ressource: Ressource)
+
+    @Query("SELECT * FROM ressource")
+    suspend fun getAllRessource() : List<Ressource>
+
+    @Query("SELECT * FROM ressource WHERE id = :id")
+    suspend fun getRessource(id:Int) : Ressource
+
+    @Insert
+    suspend fun addMultipleRessource(vararg ressource: Ressource)
+
+    @Update
+    suspend fun updateRessource(ressource: Ressource)
+
+    @Delete
+    suspend fun deleteRessourcee(ressource: Ressource)
 }
---
+++ app/src/main/java/com/example/corona/geomob/data/Domaines/Ressource.kt
@@ -0,0 +1,28 @@
+package com.example.corona.geomob.data.Domaines
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+
+@Entity(tableName = "ressource"
+    ,foreignKeys = arrayOf(
+        ForeignKey(entity = Pays::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("pays_id"),
+            onDelete = ForeignKey.CASCADE)
+    )
+)
+data class Ressource(
+
+
+    @ColumnInfo(name = "description") val description: String,
+
+    @ColumnInfo(name = "pays_id") val paysId: Int
+
+)  : Serializable {
+
+    @PrimaryKey(autoGenerate = true) var id : Int = 0
+}
