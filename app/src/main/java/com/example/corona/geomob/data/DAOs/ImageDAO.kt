2c24f82667fc2cc96c14da441cfd7ddc21841ef9
Mahi Begoug <gm_begoug@esi.dz>
2020-06-27 21:14:45 +0100

    add Image Model

---
+++ app/src/main/java/com/example/corona/geomob/data/DAOs/ImageDAO.kt
@@ -0,0 +1,31 @@
+package com.example.corona.geomob.data.DAOs
+
+import androidx.room.*
+import com.example.corona.geomob.data.Domaines.Historique
+import com.example.corona.geomob.data.Domaines.Image
+
+
+@Dao
+interface ImageDAO {
+
+    @Insert
+    suspend fun addImage(image: Image)
+
+    @Query("SELECT * FROM image ORDER BY id DESC")
+    suspend fun getAllImage() : List<Image>
+
+    @Query("SELECT * FROM image WHERE pays_id = :pays_id")
+    suspend fun findByPaysId(pays_id: Int) : List<Image>
+
+    @Query("SELECT * FROM image WHERE id = :id")
+    suspend fun getImage(id:Int) : Image
+
+    @Insert
+    suspend fun addMultipleImage(vararg image: Image)
+
+    @Update
+    suspend fun updateImage(image: Image)
+
+    @Delete
+    suspend fun deleteImage(image: Image)
+}
--- app/src/main/java/com/example/corona/geomob/data/Repository/SqlLiteDateBase.kt
+++ app/src/main/java/com/example/corona/geomob/data/Repository/SqlLiteDateBase.kt
@@ -7,12 +7,16 @@ import androidx.room.RoomDatabase
 import com.example.corona.geomob.data.DAOs.HistoriqueDAO
 import com.example.corona.geomob.data.DAOs.PaysDAO
 import com.example.corona.geomob.data.Domaines.Historique
+import com.example.corona.geomob.data.Domaines.Image
 import com.example.corona.geomob.data.Domaines.Pays
 import com.example.corona.geomob.data.Domaines.Personnalite
 import java.util.concurrent.locks.Lock
 
 
-@Database(entities = [(Pays::class) , (Historique::class), (Personnalite::class)], version = 5)
+@Database(entities = [(Pays::class),
+    (Historique::class),
+    (Personnalite::class),
+    (Image::class)], version = 5)
 abstract class SqlLiteDateBase:RoomDatabase() {
 
     abstract fun getPaysDao() :PaysDAO
